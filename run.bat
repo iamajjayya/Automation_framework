c6c262afd33a73c32b39beb7b56fdb9bd30fb4dc
Ajjayya <ajjayya2002@gmail.com>
2025-07-14 23:30:22 +0530

    updated bat file

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python -m pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+"C:\Users\ajjay\AppData\Local\Programs\Python\Python313\python.exe" -m pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
